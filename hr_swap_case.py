0d826fa39e7e1deefed08603face068aaa86dd1a
Stanislav Shchichko <stanislavshchichko@iMac-Stanislav.local>
2016-11-27 16:31:21 -0800

    sWAP cASE task solved

---
+++ hr_swap_case.py
@@ -0,0 +1,10 @@
+"""
+You are given a string S. Your task is to swap cases.
+In other words, convert all lowercase letters to uppercase letters and vice versa.
+For Example:
+Www.HackerRank.com => wWW.hACKERrANK.COM
+Pythonist 2 => pYTHONIST 2
+"""
+
+s = raw_input()
+print s.swapcase()
